741889f3e4abf9876b4411f3d49a81cd8e96ea6a
Autumn-Whisper <yeluo@hyfmwzs.com>
2023-04-06 00:38:34 +0800

    js

---
+++ footer.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var headerElement = document.getElementById('footer');
+
+    fetch(' footer.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            headerElement.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error fetching footer:', error);
+            headerElement.innerHTML = '<p>加载页脚时出错。</p>';
+        });
+});
---
+++ header.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var headerElement = document.getElementById('header');
+
+    fetch(' desktop_header.html')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.text();
+        })
+        .then(html => {
+            headerElement.innerHTML = html;
+        })
+        .catch(error => {
+            console.error('Error fetching header:', error);
+            headerElement.innerHTML = '<p>加载页头时出错。</p>';
+        });
+});
