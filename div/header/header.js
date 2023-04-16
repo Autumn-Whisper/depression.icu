8563509eac4fa499b8a09608a60fcb7a3e19b568
Autumn-Whisper <yeluo@hyfmwzs.com>
2023-04-17 00:33:08 +0800

    页头移动端适配

--- div/header/header.js
+++ div/header/header.js
@@ -1,18 +1,29 @@
 document.addEventListener('DOMContentLoaded', function () {
     var headerElement = document.getElementById('header');
-
+  
     fetch('div/header/header.html')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.text();
-        })
-        .then(html => {
-            headerElement.innerHTML = html;
-        })
-        .catch(error => {
-            console.error('Error fetching header:', error);
-            headerElement.innerHTML = '<p>加载页头时出错。</p>';
-        });
-});
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.text();
+      })
+      .then(html => {
+        headerElement.innerHTML = html;
+        setupMenuToggle();
+      })
+      .catch(error => {
+        console.error('Error fetching header:', error);
+        headerElement.innerHTML = '<p>加载页头时出错。</p>';
+      });
+  });
+  
+  function setupMenuToggle() {
+    var menuToggle = document.querySelector('.menu-toggle');
+    var menu = document.querySelector('.menu');
+  
+    menuToggle.addEventListener('click', function () {
+      menu.classList.toggle('show');
+    });
+  }
+  
