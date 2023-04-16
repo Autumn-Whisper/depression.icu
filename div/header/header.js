aed6f2b3f5acfa3b8798f7f0e90e858aea50bcb2
Autumn-Whisper <yeluo@hyfmwzs.com>
2023-04-17 01:17:19 +0800

    debug5

--- div/header/header.js
+++ div/header/header.js
@@ -1,19 +1,34 @@
 document.addEventListener('DOMContentLoaded', function () {
-    setupMenuToggle();
-  });
-  
-  function setupMenuToggle() {
-    var menuToggle = document.querySelector('.menu-toggle');
-    var menu = document.querySelector('.menu');
-  
-    menuToggle.addEventListener('click', function () {
-      menu.classList.toggle('show');
-    });
-  
-    document.addEventListener('click', function (event) {
-      if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
-        menu.classList.remove('show');
+  var headerElement = document.getElementById('header');
+
+  fetch('div/header/header.html')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      return response.text();
+    })
+    .then(html => {
+      headerElement.innerHTML = html;
+      setupMenuToggle();
+    })
+    .catch(error => {
+      console.error('Error fetching header:', error);
+      headerElement.innerHTML = '<p>加载页头时出错。</p>';
     });
-  }
-  
+});
+
+function setupMenuToggle() {
+  var menuToggle = document.querySelector('.menu-toggle');
+  var menu = document.querySelector('.menu');
+
+  menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('show');
+  });
+
+  document.addEventListener('click', function (event) {
+    if (!menu.contains(event.target) && !menuToggle.contains(event.target)) {
+      menu.classList.remove('show');
+    }
+  });
+}
